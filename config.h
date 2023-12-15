2e4dad872e14ad11906ce1ed34036f1cbeac43d5
Vinicius-P-Moraes <vinicius.moraes105@etec.sp.gov.br>
2023-12-15 04:12:32 -0300

    Andamento na função de keylogger; Foi criado um arquivo de configurações

---
+++ config.h
@@ -0,0 +1,10 @@
+/*
+#########################################
+#					#
+#	Arquivo de configurações	#
+# 					#
+#########################################
+*/
+
+//Tempo para atualizar o keylogger em segundos
+const int klog_update_time = 2;
