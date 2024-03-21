6e9fc3eafb2c5495954358fbe3685c3afc034209
Vinícius Pelatieri de Moraes <153043131+Vinicius-P-Moraes@users.noreply.github.com>
2024-03-20 22:41:01 -0300

    Create interpretador.h

---
+++ src/headers/interpretador.h
@@ -0,0 +1,15 @@
+//Header para interpretação do comando digitado pelo usuário
+#include<string>
+struct info
+{
+  std::string ip;
+  int porta;
+  int funcao;
+  bool is_valid;
+};
+
+info interpretador(char comando[command_size])
+{
+  info _info;
+  return _info;
+}
