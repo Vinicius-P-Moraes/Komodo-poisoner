159ba1cf7448b38e3bfdff8fd0bf9969660ee368
Vinícius Pelatieri de Moraes <153043131+Vinicius-P-Moraes@users.noreply.github.com>
2024-03-20 22:42:26 -0300

    Create komodo_config.h

---
+++ src/headers/komodo_config.h
@@ -0,0 +1,13 @@
+/*
+#########################################
+#					                              #
+#	Arquivo de configurações	            #
+# 					                            #
+#########################################
+*/
+
+//Tempo para atualizar o keylogger em segundos
+const int klog_update_time = 2;
+
+//tamanho máximo para o comando digitado pelo user:
+const int command_size = 100;
